1a0bbfb24a710ef56566354cfb6015190959133d
avivk <avivkeinan1@gmail.com>
2022-10-01 14:06:22 +0300

    check1

--- client/Main.java
+++ client/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         Client c = new Client("avivk9", 6969, "127.0.0.1");
-
+        c.startClient();
     }
 }
